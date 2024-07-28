39f539c073cfc04c434de79c81b237385c2c034b
FrederickThemar <jzbumgar@uark.edu>
2024-07-27 19:54:07 -0500

    Initial commite. Added main file, YOLO file, and pointcloud file.

---
+++ main.py
@@ -0,0 +1,108 @@
+import os
+import torch
+
+from tqdm import tqdm
+from yolo import yolo
+from pcd  import pcd
+
+# CLASS: creates other objects to manage the video-to-estimate pipeline
+class Main():
+    def __init__(self, path=None, depth=None, mode=None, save=False):
+        if path == None:
+            print("ERROR: filepath empty.")
+            exit(1)
+        if mode == None:
+            print("ERROR: mode empty.")
+            exit(1)
+        if not isinstance(mode, int):
+            print("ERROR: mode must be 0, 1, or 2.")
+            exit(1)
+        if mode > 2:
+            print("ERROR: mode must be 0, 1, or 2.")
+            exit(1)
+        
+        self.save = save
+        self.mode = mode
+        self.init_path = path
+        self.depth_path = depth
+
+        # Check that filepaths exist, return error otherwise
+        self.checkPath(self.init_path)
+
+        # Info for processing methods
+        self.depthOutput = "./outputDepth/"
+        self.pcdOutput = "./outputPCD/"
+        self.checkPath(self.depthOutput)
+        self.checkPath(self.pcdOutput)
+
+        # Initialize YOLO object
+        self.yolo = yolo()
+        self.pcd = pcd()
+
+        # Call class method based on mode
+        if self.mode == 0:
+            self.process_frame()
+        elif self.mode == 1:
+            self.process_video()
+        elif self.mode == 2:
+            self.process_live()
+        else:
+            print("ERROR: Incorrect mode input.")
+            print(f'Mode: {self.mode}')
+            exit(1)
+
+    # HELPER: Checks if input filepath exists.
+    def checkPath(self, inputPath):
+        if not os.path.exists(inputPath):
+            print("ERROR: Path does not exist.")
+            print(f'Path: {inputPath}')
+            exit(1)
+
+    # Estimates weight of an individual frame
+    def process_frame(self):
+        framepath = self.init_path
+
+        # Make sure depth file provided
+        if self.depth_path == None:
+            print("ERROR: For this mode, a depth frame must be provided.")
+            exit(1)
+        self.checkPath(self.depth_path)
+        
+
+        # Mask frame.
+        success = self.yolo.mask_frame(framepath, self.save)
+        if not success:
+            print("ERROR: No chicken detected in frame.")
+            exit(1)
+        
+        
+
+    # Estimates weight of all the frames in a video
+    def process_video(self):
+        # Maybe use process_frame for this? 
+        #   - Can have pframe return estimate and append to list
+        #   - Save the frames separately, then have a list storing the paths to each. Give to pframe() 
+        #       - Maybe each item in array can be another JSON, with paths to mask, depth, and color?
+        #   - TQDM can run based on number of files in frame folder
+        log("ERROR: Not yet implemented. Process video")
+
+    def process_live(self):
+        log("ERROR: Not yet implemented. Process live video.")
+
+# DEBUG: Prints debug messages. Remove later.
+def log(inputStr):
+    print(inputStr)
+
+
+# Main function
+if __name__ == '__main__':
+    print("Begin.")
+
+    # Grab user input to determine mode and filepath
+    TEMP_path = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240326/chicken3/color/000100.jpg'
+    TEMP_depth = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240326/chicken3/depth/000100.png'
+
+    # Initialize Main object to handle the pipeline
+    main = Main(path=TEMP_path, depth=TEMP_depth, mode=0)
+
+    # Begin the pipeline
---
+++ yolo.py
@@ -0,0 +1,92 @@
+import os
+import torch
+import cv2
+import numpy as np
+
+from tqdm import tqdm
+from ultralytics import YOLO
+# from main import log
+
+class yolo():
+    def __init__(self):
+        print("Initializing YOLO model... ", end="", flush=True)
+
+        self.modelName = "20240615_med15"
+
+        # Check output path
+        self.maskOutput = "./outputMask/"
+        self.checkPath(self.maskOutput)
+
+        # Initialize model
+        self.model = YOLO(f'./resources/{self.modelName}.pt')
+
+        # Switch model to use CUDA
+        torch.cuda.set_device(0)
+        self.model.to('cuda')
+
+        # Where filepaths to output masks are saved
+        self.maskPaths = []
+
+        print("Done!")
+
+    def mask_frame(self, path, save):
+        self.frame_path = path
+        
+        results = self.model(self.frame_path, conf=0.80)
+
+        success = False
+        for result in results:
+            mask = result.masks
+            if mask is None:
+                continue
+            else:
+                success = True
+
+            origPath = result.path
+            origEnd = origPath.split('/')[-1].split('.')[0]
+
+            if save:
+                # NOTE: Change this later to save to different directory
+                overlayPath = f'{self.maskOutput}/overlay/{origEnd}.jpg'
+                result.save(filename=overlayPath)
+
+            # Isolate masks on a blank image
+            img = np.copy(result.orig_img) # Original color frame
+            b_mask = np.zeros(img.shape[:2], np.uint8) # Blank image
+
+            # Get each mask on image
+            labels = []
+            for ci, c in enumerate(result):
+                label = c.names[c.boxes.cls.tolist().pop()]
+                labels.append(label)
+
+                # Create contour mask
+                contour = c.masks.xy.pop().astype(np.int32).reshape(-1, 1, 2)
+                _ = cv2.drawContours(b_mask, [contour], -1, (255,255,255), cv2.FILLED)
+
+            # Draw part mask covers onto blank image
+            mask3ch = cv2.cvtColor(b_mask, cv2.COLOR_GRAY2BGR)
+            isolated = cv2.bitwise_and(mask3ch, img)
+
+            # Save isolated mask into temp folder
+            maskOutputDir = f'{self.maskOutput}/{origEnd}.png'
+            cv2.imwrite(maskOutputDir, isolated)
+
+            self.maskPaths.append(maskOutputDir)
+
+        return success
+
+    def mask_video(self, path):
+        self.video_path = path
+        print(self.video_path)
+
+    def mask_live(self):
+        # Not sure how to do this one. Maybe the Main object should save some frames from video then call this?
+        pass
+
+    # HELPER: Checks if input filepath exists.
+    def checkPath(self, inputPath):
+        if not os.path.exists(inputPath):
+            print("ERROR: Path does not exist.")
+            print(f'Path: {inputPath}')
+            exit(1)
