597af5e05c9098da5559a89bd587093e93ffc032
FrederickThemar <jzbumgar@uark.edu>
2024-08-08 15:12:58 -0500

    process_video now shows a live visual of the frames that have been most recently processed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import torch
 
 import open3d as o3d
+import numpy as np
 
 from tqdm import tqdm
 from yolo import yolo
@@ -70,18 +71,23 @@ class Main():
             exit(1)
         self.checkPath(self.depth_path)
         depthpath = self.depth_path
-        depth = cv2.imread(self.depth_path, flags=cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        # depth = cv2.imread(self.depth_path, flags=cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+        depth = o3d.io.read_image(self.depth_path)
+        # rgb_im = o3d.io.read_image(self.color_path)
 
         # Mask frame.
-        success, mask = self.yolo.mask_frame(color, self.save)
+        success, mask, _ = self.yolo.mask_frame(color, self.save)
         if not success:
             print("ERROR: No chicken detected in frame.")
             exit(1)
         
         pcd = self.pcd.pcd_frame(color, depth, mask, self.save)
+        if pcd == None:
+            print("\nERROR: The mask for this frame falls outside the acceptable boundaries for the model. Try a different frame.")
+            exit(1)
         output = self.kpconv.estimate_frame(pcd)
 
-        print(f'OUTPUT:\t\t\n{output[0][0]}')
+        print(f'\nOUTPUT:\t\t\n{output[0][0]}')
 
     # Estimates weight of all the frames in a video
     def process_video(self):
@@ -102,22 +108,89 @@ class Main():
         reader.open(videopath)
 
         count = 0
+        outputs = []
         while not reader.is_eof():
             rgbd = reader.next_frame()
             if rgbd is None:
                 continue
 
             # Store the color and depth frames
-            color = rgbd.color
+            # color = rgbd.color
             depth = rgbd.depth
+            
+            # This might not be needed!
+            DEBUG_depthCV2 = np.asarray(depth)
+            DEBUG_depthCV2 = cv2.cvtColor(DEBUG_depthCV2, cv2.COLOR_RGB2BGR)
+            color = np.asarray(rgbd.color)
+            color = cv2.cvtColor(color, cv2.COLOR_RGB2BGR)
+            # print(DEBUG_depthCV2.dtype)
+            # cv2.imwrite('img.png',DEBUG_depthCV2)
+            # o3d.io.write_image('img2.png', depth)
+            # cv2.waitKey(0)
+
+            success, mask, overlay = self.yolo.mask_frame(color, self.save)
+            if not success:
+                # Write frame count
+                color = cv2.rectangle(color, (0, 0), (250, 50), (200,0,0), -1)
+                color = cv2.putText(
+                    color,
+                    f'Frame: {count+1}',
+                    (15, 35),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.imshow('frame', color)
+                cv2.waitKey(10)
+                
+                count+=1
+                continue
+
+            pcd = self.pcd.pcd_frame(color, depth, mask, self.save)
+            if pcd == None:
+                # Write frame count
+                color = cv2.rectangle(color, (0, 0), (250, 50), (200,0,0), -1)
+                color = cv2.putText(
+                    color,
+                    f'Frame: {count+1}',
+                    (15, 35),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.imshow('frame', color)
+                cv2.waitKey(10)
+                
+                count+=1
+                continue
+
+            output = self.kpconv.estimate_frame(pcd)
+            outputs.append(output)
+            
+            # Write output on visualization frame
+            overlay = cv2.rectangle(overlay, (0, 0), (250, 50), (200,0,0), -1)
+            overlay = cv2.putText(
+                overlay,
+                f'Frame: {count+1}',
+                (15, 35),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            overlay = cv2.rectangle(overlay, (0, 50), (250, 100), (0,200,0), -1)
+            overlay = cv2.putText(
+                overlay,
+                'Output: %.2f' % round(output[0][0],2),
+                (15, 85),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+            # Display the visualization frame
+            cv2.imshow('frame', overlay)
+            cv2.waitKey(10)
 
             count+=1
+            # break
         print(f'COUNT: {count}')
+        print(f'LEN:   {len(outputs)}')
+        print(f'AVG:   {np.mean(outputs)}')
 
         # Close the video
         reader.close()
 
-        print(videopath)
+        # Close cv2 window
+        cv2.destroyAllWindows()
 
     def process_live(self):
         log("ERROR: Not yet implemented. Process live video.")
@@ -136,14 +209,15 @@ if __name__ == '__main__':
     # TEMP_path = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240326/chicken3/color/000100.jpg'
     # TEMP_depth = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240326/chicken3/depth/000100.png'
     # Acceptable mask
-    TEMP_path = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240409/chicken20/color/000098.jpg'
-    TEMP_depth = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240409/chicken20/depth/000098.png'
+    # TEMP_path = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240409/chicken20/color/000098.jpg'
+    # TEMP_depth = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Depth/Spring2024/20240409/chicken20/depth/000098.png'
 
-    # TEMP_video = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Spring2024/20240409/chicken_13.mkv'
+    TEMP_video = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Spring2024/20240409/chicken_16.mkv'
+    # TEMP_video = '/mnt/khoavoho/datasets/chicken_weight_dataset/jzbumgar/Spring2024/20240306/chicken_16.mkv'
 
     # Initialize Main object to handle the pipeline
-    main = Main(path=TEMP_path, depth=TEMP_depth, mode=0)
-    # main = Main(path=TEMP_video, mode=1)
+    # main = Main(path=TEMP_path, depth=TEMP_depth, mode=0)
+    main = Main(path=TEMP_video, mode=1)
 
     # Begin the pipeline
     main.begin()
